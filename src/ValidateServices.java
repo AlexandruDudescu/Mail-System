18a434c1e39702432437e46b58e17054bcefeba5
ReedS0007 <rts0007@uah.edu>
2017-03-06 23:20:47 -0600

    Add files via upload

---
+++ src/ValidateServices.java
@@ -0,0 +1,31 @@
+/**
+ * Class to validate the inputs of the user.
+ * @author Reed Schoon
+ *
+ */
+
+public class ValidateServices 
+{
+	/**
+	 * Validates an account to make sure it is unique.
+	 * @author Reed Schoon
+	 * @param account
+	 * @return true if account is unique, false if account is not unique
+	 */
+	public boolean ValidateAccount( String account )
+	{
+		return true;
+	}
+
+	/**
+	 * Validates a username to make sure it is unique.
+	 * @author Reed Schoon
+	 * @param account
+	 * @return true if account is unique, false if account is not unique
+	 */	
+	public boolean ValidateUsername( String username ) 
+	{
+		return true;
+	}
+	
+}
