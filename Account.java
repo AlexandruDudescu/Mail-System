e8e3852a251af2ab613edd9ed6afb81657b62c31
WangKevin9142 <kw0029@uah.edu>
2017-03-06 20:46:46 -0600

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Account 
+{
+	public Account(String name)
+	{
+		name = emailName;
+	}
+	
+	public void LocalAccount()
+	{
+		
+	}
+	
+	public void RemoteAccount()
+	{
+		
+	}
+	
+	private String emailName;
+}
