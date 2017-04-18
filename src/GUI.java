5bfe7f21b9d42a417556b1425b3a9c4bae953804
ReedS0007 <rts0007@uah.edu>
2017-04-17 20:28:57 -0500

    Add files via upload

--- src/GUI.java
+++ src/GUI.java
@@ -1,11 +1,10 @@
 import java.awt.GridLayout;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -17,6 +16,10 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -29,31 +32,51 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
+@SuppressWarnings("serial")
 public class GUI extends JComponent implements TreeSelectionListener 
 {
-	JFrame frame = new JFrame("Voice Mail System - GUI Prototype");
-	//JFrame messageFrame = new JFrame();
-	JFrame composeFrame = new JFrame("Compose Email");
-	JFrame replyFrame = new JFrame("Reply Email");
+//Menu variables
+	JMenuBar menuBar;
+	JMenu accounts;
+	JMenuItem addAccount;
+	JMenuItem removeAccount;
+//Frame variables
+	JFrame frame = new JFrame("Simple Email - Team Void");
+	JFrame composeFrame = null;
+	JFrame replyFrame = null;
+//Panel variables	
 	JPanel buttonPanel = new JPanel();
+	JPanel textPanel = new JPanel();
+	JPanel labelPanel = new JPanel();	
+//Button variables	
 	JButton composeButton = new JButton("Compose");
-	JTree tree;
 	JButton replyButton = new JButton("Reply");
 	JButton sendButton = new JButton ("Send");
 	JButton removeButton = new JButton("Remove");
+//Text Field variables
+	JTextField fromTextField = new JTextField(30);
+	JTextField toTextField = new JTextField(30);
+	JTextField subjectTextField = new JTextField(30);
+//Text Area variables
 	JTextArea screen = new JTextArea(10, 10);
-	JPanel textPanel = new JPanel();
-	JPanel labelPanel = new JPanel();
+    JTextArea bodyTextArea = new JTextArea(10,30);
+//Tree & Scroll Pane variables
+	JTree tree;
 	JScrollPane treeView;
 	JScrollPane screenScroller = new JScrollPane(screen);
-	//String data = null;
-	JTextField firstField = new JTextField(30);
-	JTextField secondField = new JTextField(30);
-    JTextArea thirdField = new JTextArea(10,30);
-    Date now = new Date();
 	
-	GUI () 
-	{		
+    Date now = null;
+	
+	GUI() 
+	{
+		//Menu code
+		menuBar = new JMenuBar();
+		accounts = new JMenu("Account");
+		addAccount = new JMenuItem("Add Account");
+		removeAccount = new JMenuItem("Remove Account");
+		menuBar.add(accounts);
+		accounts.add(addAccount);
+		accounts.add(removeAccount);
 		//Button panel code
 		buttonPanel.setLayout(new GridLayout(1, 2));
 		buttonPanel.add(composeButton);
@@ -62,57 +85,178 @@ public class GUI extends JComponent implements TreeSelectionListener
 		buttonPanel.add(removeButton);
 		buttonPanel.setBackground(Color.lightGray);
 		
-		//
+		//Grid code
 		GridLayout gridLayout = new GridLayout(1, 2, 50, 50);
 		gridLayout.setHgap(10);
 		gridLayout.setVgap(10);
 		labelPanel.setLayout(gridLayout);
-	
-		screen.setBackground(Color.yellow);
+
+		screen.setBackground(Color.lightGray);
 		screen.setBorder(BorderFactory.createBevelBorder(1));
 		
 		textPanel.setBorder(new EmptyBorder(10,10,10,10));
 		textPanel.setLayout(gridLayout);
 		textPanel.add(screenScroller);
 		
+		bodyTextArea.setLineWrap(true);
+		
+		//Menu functionality
+		addAccount.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event)
+			{
+				//TODO: add menu button functionality
+				System.out.println("Test: Add");
+			}
+		});
+		
+		removeAccount.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event)
+			{
+				//TODO: add menu button functionality
+				System.out.println("Test: Remove");
+			}
+		});
 		
 		//Button functionality
 		composeButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent event)
 			{
-				thirdField.setLineWrap(true);
+				if( composeFrame != null ) //if frame is open
+				{
+					//close frame
+					composeFrame.setVisible(false);
+					composeFrame.dispose();
+					composeFrame = null;
+				}
+						
+				if( replyFrame != null ) //if frame is open
+				{
+					//close frame
+					replyFrame.setVisible(false);
+					replyFrame.dispose();	
+					replyFrame = null;
+				}
+				//clear text fields
+	        	fromTextField.setText("");
+	        	toTextField.setText("");
+	        	subjectTextField.setText("");
+	        	bodyTextArea.setText("");
+	        	
+	        	//create new frame
+				composeFrame = new JFrame("Compose Email");
 				composeFrame.setLayout(new FormLayout());
-				composeFrame.setTitle("Compose Email");
-				composeFrame.add(new JLabel("To"));
-				composeFrame.add(firstField);
-				composeFrame.add(new JLabel("Subject"));
-				composeFrame.add(secondField);
+				composeFrame.add(new JLabel("From:"));
+				composeFrame.add(fromTextField);
+				composeFrame.add(new JLabel("To:"));
+				composeFrame.add(toTextField);
+				composeFrame.add(new JLabel("Subject:"));
+				composeFrame.add(subjectTextField);
 				composeFrame.add(new JLabel(""));
-				composeFrame.add(thirdField);
-				composeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				composeFrame.add(bodyTextArea);
+				composeFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); 
+				//DO_NOTHING_ON_CLOSE is needed for the following addWindowListener
+				composeFrame.setResizable(false);
 				composeFrame.pack();
 				composeFrame.setVisible(true);
+				//Prompt the user on close if text was written in any of the fields
+				composeFrame.addWindowListener(new java.awt.event.WindowAdapter() 
+				{
+				    @Override
+				    public void windowClosing(java.awt.event.WindowEvent windowEvent) 
+				    {
+				    	if( !fromTextField.getText().isEmpty() || !toTextField.getText().isEmpty() || !subjectTextField.getText().isEmpty() || !bodyTextArea.getText().isEmpty() )
+				    	{
+					        if (JOptionPane.showConfirmDialog(frame, 
+					            "Are you sure to close this window? Anything you've written won't be saved or sent.", "Close Confirmation", 
+					            JOptionPane.YES_NO_OPTION,
+					            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
+					        {
+					        	composeFrame.setVisible(false);
+					        	composeFrame.dispose();
+					        	composeFrame = null;				        	
+					        }
+				    	}
+				    	else
+				    	{
+				        	composeFrame.setVisible(false);
+				        	composeFrame.dispose();
+				        	composeFrame = null;
+				    	}
+				    }
+				});
 			}
-			
 		});
 		
 		replyButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent event)
 			{
-				thirdField.setLineWrap(true);
+				if( composeFrame != null ) //if frame is open
+				{
+					//close frame
+					composeFrame.setVisible(false);
+					composeFrame.dispose();
+					composeFrame = null;
+				}
+						
+				if( replyFrame != null ) //if frame is open
+				{
+					//close frame
+					replyFrame.setVisible(false);
+					replyFrame.dispose();
+					replyFrame = null;
+				}
+				//clear text fields
+	        	fromTextField.setText("");
+	        	toTextField.setText("");
+	        	subjectTextField.setText("");
+	        	bodyTextArea.setText("");
+				
+	        	//create new frame
+				replyFrame = new JFrame("Reply Email");
 				replyFrame.setLayout(new FormLayout());
-				replyFrame.setTitle("Reply Email");
-				replyFrame.add(new JLabel("To"));
-				replyFrame.add(new JTextField(30));
-				replyFrame.add(new JLabel("Subject"));
-				replyFrame.add(new JTextField(30));
+				replyFrame.add(new JLabel("From:"));
+				replyFrame.add(fromTextField);
+				replyFrame.add(new JLabel("To:"));
+				replyFrame.add(toTextField);
+				replyFrame.add(new JLabel("Subject:"));
+				replyFrame.add(subjectTextField);
 				replyFrame.add(new JLabel(""));
-				replyFrame.add(thirdField);
-				replyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				replyFrame.add(bodyTextArea);
+				replyFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+				//DO_NOTHING_ON_CLOSE is needed for the following addWindowListener
+				replyFrame.setResizable(false);
 				replyFrame.pack();
 				replyFrame.setVisible(true);
+				//Prompt the user on close if text was written in any of the fields
+				replyFrame.addWindowListener(new java.awt.event.WindowAdapter() 
+				{
+				    @Override
+				    public void windowClosing(java.awt.event.WindowEvent windowEvent) 
+				    {
+				    	if( !fromTextField.getText().isEmpty() || !toTextField.getText().isEmpty() || !subjectTextField.getText().isEmpty() || !bodyTextArea.getText().isEmpty() )
+				    	{
+					        if (JOptionPane.showConfirmDialog(frame, 
+					            "Are you sure to close this window? Anything you've written won't be saved or sent.", "Close Confirmation", 
+					            JOptionPane.YES_NO_OPTION,
+					            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
+					        {
+					        	replyFrame.setVisible(false);
+					        	replyFrame.dispose();
+					        	replyFrame = null;				        	
+					        }
+				    	}
+				    	else
+				    	{
+				    		replyFrame.setVisible(false);
+				    		replyFrame.dispose();
+				    		replyFrame = null;
+				    	}
+				    }				    
+				});
 			}
 		});
 		
@@ -120,15 +264,72 @@ public class GUI extends JComponent implements TreeSelectionListener
 		{
 			public void actionPerformed(ActionEvent event)
 			{
-				Email newEmail = new Email();
-				toField.setServerDomain(firstField.getText());		//Probably have to change this
-				subjectField = secondField.getText();
-				bodyField = thirdField.getText();
-				newEmail = EmailManager.CreateEmail(toField, senderField, subjectField, bodyField, now);
-				EmailManager.sendEmail(newEmail);
-		        System.out.println(toField);
-		        System.out.println(subjectField);
-		        System.out.println(bodyField);
+				//check if a frame is open
+				if( composeFrame != null || replyFrame != null )
+				{
+					ArrayList<Account> accountClone = AccountManager.getAccountList();
+					String[] toParts = toTextField.getText().split("@", 2);
+					String[] fromParts = fromTextField.getText().split("@", 2);
+					boolean hasFoundToName = false;
+					boolean hasFoundFromName = false;
+					int i = 0;
+					int j = 0;
+					for( i = 0; i < accountClone.size(); i++ )
+					{
+						if( accountClone.get(i).getEmailName().equals(toParts[0]) )
+						{
+							hasFoundToName = true;
+							break;
+						}
+					}
+					
+					for( j = 0; j < accountClone.size(); j++ )
+					{
+						if( accountClone.get(j).getEmailName().equals(fromParts[0]) )
+						{
+							hasFoundFromName = true;
+							break;
+						}
+					}
+					
+					if( hasFoundToName == false || hasFoundFromName == false || ( !toParts[1].equals("local.com") && !toParts[1].equals("remote.com") ) )
+					{
+						JOptionPane.showMessageDialog(frame, 
+					            "Cannot find the specified email address.", "Error", 
+					            JOptionPane.OK_OPTION);
+						return;
+					}
+					//TODO: add dynamic check to make sure the server exists
+					//Right now, this deals only with the test cases "local.com" and "remote.com"
+					if( ( toParts[1].equals("local.com") || toParts[1].equals("remote.com") ) && (fromParts[1].equals("local.com") || fromParts[1].equals("remote.com") ) )
+					{
+						if( toParts[1].equals("local.com") )
+						{
+							toEmailAddress = accountClone.get(i).getLocalAddresses().get(0);
+						}
+						else //remote.com
+						{
+							toEmailAddress = accountClone.get(i).getRemoteAddresses().get(0);
+						}
+						
+						if( fromParts[1].equals("local.com") )
+						{
+							senderEmailAddress = accountClone.get(i).getLocalAddresses().get(0);
+						}
+						else //remote.com
+						{
+							senderEmailAddress = accountClone.get(i).getRemoteAddresses().get(0);
+						}
+						subjectString = subjectTextField.getText();
+						bodyString = bodyTextArea.getText();
+						now = new Date();
+						Email newEmail = EmailManager.CreateEmail(toEmailAddress, senderEmailAddress, subjectString, bodyString, now);
+						EmailManager.sendEmail(newEmail);
+				        System.out.println(toTextField.getText());
+				        System.out.println(subjectString);
+				        System.out.println(bodyString);
+					}
+				}
 			}
 		});
 		
@@ -142,44 +343,47 @@ public class GUI extends JComponent implements TreeSelectionListener
 		    	 String temp = null;
 		         int selRow = tree.getRowForLocation(e.getX(), e.getY());
 		         TreePath selPath = tree.getPathForLocation(e.getX(), e.getY());
-		         temp = selPath.toString();
-		         char[] charArray = temp.toCharArray();
-		         char[] tempArray = new char[30];
-		         int commaCount = 0;
-		         int j = 0;
-		         if(selRow != -1 && e.getClickCount() == 2) 
+		         if( selPath != null )
 		         {
-		        	 for(int i = 0; i < charArray.length; i++)
-		        	 {
-		        		 String charHolder = Character.toString(charArray[i]);
-		        		 if(commaCount == 3)
-		        		 {
-		        			 if(!charHolder.equals("]"))
-		        			 {
-			        			 tempArray[j] = charArray[i];
-			        			 j++;
-		        			 }
-		        		 }
-		        		 if(charHolder.equals(","))
-		        		 {
-		        			 commaCount++;		        		
-		        		 }
-		        	 }
-		        	if(commaCount == 3)
-		        	{
-			        	 String b = new String(tempArray);
-			        	 System.out.println(b);
-		         		 //senderField.setServerDomain(b);
-		        	}		           
+			         temp = selPath.toString();
+			         char[] charArray = temp.toCharArray();
+			         char[] tempArray = new char[30];
+			         int commaCount = 0;
+			         int j = 0;
+			         if(selRow != -1 && e.getClickCount() == 2) 
+			         {
+			        	 for(int i = 0; i < charArray.length; i++)
+			        	 {
+			        		 String charHolder = Character.toString(charArray[i]);
+			        		 if(commaCount == 3)
+			        		 {
+			        			 if(!charHolder.equals("]"))
+			        			 {
+				        			 tempArray[j] = charArray[i];
+				        			 j++;
+			        			 }
+			        		 }
+			        		 if(charHolder.equals(","))
+			        		 {
+			        			 commaCount++;		        		
+			        		 }
+			        	 }
+			        	if(commaCount == 3)
+			        	{
+				        	 String b = new String(tempArray);
+				        	 System.out.println(b);
+			         		 //senderField.setServerDomain(b);
+			        	}		           
+			         }
 		         }
 		     }
 		 };
 		
 		
-		//tree logic
-		
-		
-		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Accounts");
+		//Tree logic
+		 
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Accounts" +
+		"                                                                            "); //spacing
 		treeView = new JScrollPane();
 		
 		tree = new JTree(root);
@@ -192,13 +396,14 @@ public class GUI extends JComponent implements TreeSelectionListener
         
         tree.addMouseListener(ml);
         
+        //Frame logic
         
 		frame.setSize(1000, 500);
-		//frame.add(composeButton);
 		frame.add(buttonPanel, BorderLayout.NORTH);
 		frame.add(textPanel, BorderLayout.CENTER);
 		frame.add(labelPanel, BorderLayout.SOUTH);
 		frame.add(treeView, BorderLayout.WEST);
+		frame.setJMenuBar(menuBar);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
@@ -209,9 +414,8 @@ public class GUI extends JComponent implements TreeSelectionListener
 	{
 	}
 	
-	private EmailAddress toField;
-	private EmailAddress senderField;
-	private String subjectField;
-	private String bodyField;
-	//private Point mousePoint;
+	private EmailAddress toEmailAddress;
+	private EmailAddress senderEmailAddress;
+	private String subjectString;
+	private String bodyString;
 }
